ed084900b653d27e70a3b6a9464027ec3032c8a5
KIM SANGYEUB <kim.sangyeub@rooter.co.jp>
2019-06-05 02:11:20 +0900

    add revision in SearchRegisteredTicketUI

--- SearchRegisteredTicketUI.cpp
+++ SearchRegisteredTicketUI.cpp
@@ -1,6 +1,19 @@
 // Class: SearchRegisteredTicketUI
 // Description: 등록된 티켓 조회를 위한 boundary class이다.
 // Author: 김상엽
+// Created: 2019/05/31
+// Mail: natsuko0012@gmail.com
+
+// Revisions:
+// 		1.	When & Who: 2019/05/31 by 김상엽
+// 				What: create file
+//
+// 		2.	When & Who: 2019/05/31 by 김상엽
+// 				What: complete basic logic with SearchRegisterTicket
+//
+// 		3.	When & Who: 2019/06/01 by 김상엽
+// 				What: add Korean comment and modify some miss on var name
+
 #include "SearchRegisteredTicketUI.h"
 #include "SearchRegisteredTicket.h"
 
