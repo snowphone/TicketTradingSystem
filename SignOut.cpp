d9ebd9186452b9568d737d0dd4379e055f5d3ee4
KIM SANGYEUB <kim.sangyeub@rooter.co.jp>
2019-06-06 02:31:43 +0900

    add function revision on SignOut.cpp

--- SignOut.cpp
+++ SignOut.cpp
@@ -1,6 +1,6 @@
-//Class: SignOut
-//Description: 로그아웃을 담당하는 control class이다.
-//Author: 김상엽
+// Class: SignOut
+// Description: 로그아웃을 담당하는 control class이다.
+// Author: 김상엽
 // Created: 2019/06/03
 // Mail: natsuko0012@gmail.com
 
@@ -18,7 +18,16 @@
 #include "SignOut.h"
 #include <iostream>
 
+// Function: SignOut::SignOut()
+// Description: SignOut 클래스의 생성자이다.
+// Author: 김상엽
+// Created: 2019/06/03
+// Parameters:
+// Return Value:
 
+// Revisions:
+// 		1.	When & Who: 2019/06/03 by 김상엽
+// 				What: create function
 
 SignOut::SignOut()
 {
@@ -33,12 +42,40 @@ SignOut & SignOut::get()
 	return *var;
 }
 
+// Function: void SignOut::requestSignOut(const Info & info)
+// Description: 로그아웃을 하는 함수이다.
+// Author: 김상엽
+// Created: 2019/06/03
+// Parameters: const Info & info(로그아웃할 계정의 정보)
+// Return Value:
+
+// Revisions:
+// 		1.	When & Who: 2019/06/03 by 김상엽
+// 				What: create function
+//
+// 		2.	When & Who: 2019/06/03 by 김상엽
+// 				What: add some test code
+//
+// 		3.	When & Who: 2019/06/03 by 김상엽
+// 				What: remove test code and complete logic
+
 void SignOut::requestSignOut(const Info & info)
 {
 
 	std::cout << "2.2 로그아웃" << std::endl
 		<< "> " << info.getID() << std::endl;
 }
+
+// Function: SignOut::~SignOut()
+// Description: SignOut 클래스의 소멸자이다.
+// Author: 김상엽
+// Created: 2019/06/03
+// Parameters:
+// Return Value:
+
+// Revisions:
+// 		1.	When & Who: 2019/06/03 by 김상엽
+// 				What: create function
 
 SignOut::~SignOut()
 {
