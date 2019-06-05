2abc8e3be57edca029968b79c0d90c9923d316aa
soyeon27 <51394144+soyeon27@users.noreply.github.com>
2019-06-06 01:02:08 +0900

    Update BookedTicketManagerUI.cpp

--- BookedTicketManagerUI.cpp
+++ BookedTicketManagerUI.cpp
@@ -1,6 +1,17 @@
 // Class: BookedTicketManagerUI
 // Description: 예약된 티켓 조회를 위한 Boundary class이다.
 // Author: 김소연
+// Created: 2019/06/01
+// Mail: 96syk@naver.com
+
+// Revisions:
+// 		1.	When & Who: 2019/06/01 by 김소연
+// 				What: create file
+//
+// 		2.	When & Who: 2019/06/04 by 김소연
+// 				What: modify some errors
+
+
 #include "BookedTicketManagerUI.h"
 #include "BookedTicketManager.h"
 
