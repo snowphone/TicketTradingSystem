f72ed5f4e9b8ef40b33c9f2d843b3b2b27c41256
soyeon27 <51394144+soyeon27@users.noreply.github.com>
2019-06-06 01:23:48 +0900

    Update SignInUI.cpp

--- SignInUI.cpp
+++ SignInUI.cpp
@@ -1,6 +1,15 @@
 //Class: SignInUI
 //Description: 로그인 기능을 담당하는 boundary 클래스이다.
 //Author: 김소연
+// Created: 2019/06/01
+// Mail: 96syk@naver.com
+
+// Revisions:
+// 		1.	When & Who: 2019/06/01 by 김소연
+// 				What: create file
+//
+// 		2.	When & Who: 2019/06/03 by 김소연
+// 				What: modify some errors and add comments
 #include "SignInUI.h"
 #include "SignIn.h"
 
@@ -16,12 +25,12 @@ SignInUI & SignInUI::get()
 {
 	if (!var)
 		var = new SignInUI();
-	return *var;
+	return *var; //instance caller
 }
 
 const Info * SignInUI::requestSignIn(std::string id, std::string pw)
 {
-	return SignIn::get().putIdPw(id, pw);
+	return SignIn::get().putIdPw(id, pw); //로그인화면 UI로 제공
 }
 
 SignInUI::~SignInUI()
