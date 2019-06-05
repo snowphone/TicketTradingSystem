39c60d1f9d504b2633422cf3c241b11bc73572f1
sram <birdram@gmail.com>
2019-06-06 00:53:05 +0900

    Buyer, registerticket manager&UI

--- Buyer.cpp
+++ Buyer.cpp
@@ -1,45 +1,73 @@
 // Class: Buyer
 // Descripton: 구매자 객체를 표현하는 클래스이다. 
 // Author: 유새람
-
-// Revisions:
-// 		1.	When & Who: 2019/06/02 by 유새람
-// 				What: create file
-//
-// 		2.	When & Who: 2019/06/02 by 유새람
-// 				What: complete basic logic
-//
-// 		3.	When & Who: 2019/06/04 by 유새람
-// 				What: add comment
-
 #include "Buyer.h"
 
-
+// Function: const Info & Buyer::getInfo() const
+// Description: Buyer의 정보를 얻어오는 함수
+// Return Value: Buyer의 info
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
 const Info & Buyer::getInfo() const
 {	
-	return this->info;	//info(사용자정보) get
+	return this->info;	
 }
 
+
+// Function: vector<std::shared_ptr<Ticket>>& Buyer::getTickets()
+// Description: Buyer가 구매한 티켓들의 정보를 가져오는 함수
+// Return Value: ticket collection
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
+//				What: vector를 사용해 가진 ticket들을 가져옴
 std::vector<std::shared_ptr<Ticket>>& Buyer::getTickets()
 {
-	return tickets;		//tickets get
+	return tickets;		
 }
 
-
+// Function: bool Buyer::empty() const
+// Description: Buyer가 티켓을 가지고있는지 확인하는 함수
+// Return Value: Buyer의 티켓 유무 여부 (bool)
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
+//				What: tickets.empty() 호출
 bool Buyer::empty() const
 {
-	return tickets.empty();	//buyer(구매자)가 티켓을 가지고있는지 확인
+	return tickets.empty();	
 }
 
+// Function: bool Buyer::operator==(const Info& i) const
+// Description: info instance로부터 Buyer를 찾기위해 사용하는 함수 
+// Return Value: 비교연산 (bool)
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
+//				What: 연산자 오버로딩 사용
 bool Buyer::operator==(const Info& i) const
 {
 	return this->info == i;	//info 인스턴스 중 buyer를 찾기 위한 보조함수
 }
 
+// Function: Buyer(const Info & info) : info(info)
+// Description: info를 사용해 Buyer instance 생성하는 constructor
+// Parameters: info- Buyer의 정보
+// Return Value: Buyer instance
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
+//				What: constructor 사용
 Buyer::Buyer(const Info & info) : info(info)
 {
 }
 
+// Function: ~Buyer()
+// Description: 소멸자
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
 Buyer::~Buyer()
 {
 }
--- RegisterTicketUI.cpp
+++ RegisterTicketUI.cpp
@@ -5,13 +5,25 @@
 #include "RegisterTicketManager.h"
 
 
-
+// Function: RegisterTicketUI()
+// Description: private 생성자(singleton)
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
 RegisterTicketUI::RegisterTicketUI()
 {
 }
 
 
 RegisterTicketUI* RegisterTicketUI::var = nullptr;
+
+// Function: static RegisterTicketUI& get();
+// Description: singleton패턴에서의 instance를 불러오는 caller
+// Return Value: RegisterTicketUI
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
+//				What: 존재하지 않으면 새롭게 생성/존재하면 RegisterTicketUI return
 RegisterTicketUI& RegisterTicketUI::get()
 {
 	if (!var)
@@ -19,12 +31,27 @@ RegisterTicketUI& RegisterTicketUI::get()
 	return *var;
 }
 
+// Function: void createNewTicket(const Info* user, int price, std::string time, std::string home, std::string away, std::string position, bool useLTA);
+// Description: 판매자가 등록한 티켓을 새로 추가하기위한  boundary method
+// Parameters:	info * user- 등록하려는 Seller
+//				int price- 희망 판매가격
+//				string time, home, away, position- 경기정보(경기 날짜, 홈팀, 어웨이팀, 좌석 위치)
+//				bool useLTA- limited-time auction 사용 여부
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
+//				What: RegisterTicketManager의 addNewTicket 함수 호출
 void RegisterTicketUI::createNewTicket(const Info* user, int price, std::string time, 
 	std::string home, std::string away, std::string position, bool useLTA)
 {
 	RegisterTicketManager::get().addNewTicket(user, price, time, home, away, position, useLTA);
 }
 
+// Function: ~RegisterTicketUI()
+// Description: 소멸자
+// Author: 유새람
+// Revisions:
+//		1. When & Who: 2019/06/02 by 유새람
 RegisterTicketUI::~RegisterTicketUI()
 {
 }
