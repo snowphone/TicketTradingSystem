8906524ce6205e3d1bcd77044af5b45d985252a6
soyeon27 <51394144+soyeon27@users.noreply.github.com>
2019-06-06 20:23:42 +0900

    Update BookedTicketManagerUI.cpp

--- BookedTicketManagerUI.cpp
+++ BookedTicketManagerUI.cpp
@@ -7,22 +7,34 @@
 // Revisions:
 // 		1.	When & Who: 2019/06/01 by 김소연
 // 				What: create file
-//
-// 		2.	When & Who: 2019/06/04 by 김소연
-// 				What: modify some errors
 
 
 #include "BookedTicketManagerUI.h"
 #include "BookedTicketManager.h"
 
 
-
+// Function: BookedTicketManagerUI()
+// Description: private 생성자(singleton)
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 BookedTicketManagerUI::BookedTicketManagerUI()
 {
 }
 
 
+// Function: static BookedTicketManagerUI* var
+// Description: private instance변수(singleton)
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 BookedTicketManagerUI* BookedTicketManagerUI::var = nullptr;
+
+// Function: static BookedTicketManagerUI& get();
+// Description: instance caller(singleton)
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 BookedTicketManagerUI & BookedTicketManagerUI::get()
 {
 	if (!var)
@@ -30,11 +42,22 @@ BookedTicketManagerUI & BookedTicketManagerUI::get()
 	return *var;
 }
 
+// Function: void search(const Info& buyerInfo);
+// Description: 예약 티켓 조회를 위한 boundary method
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 void BookedTicketManagerUI::search(const Info & buyerInfo)
 {
 	BookedTicketManager::get().print(buyerInfo);
 }
 
+
+// Function: ~BookedTicketManagerUI()
+// Description: 소멸자
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 BookedTicketManagerUI::~BookedTicketManagerUI()
 {
 }
