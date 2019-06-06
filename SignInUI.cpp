d33afd404146898a9ccddf86afefe73c5f68a677
soyeon27 <51394144+soyeon27@users.noreply.github.com>
2019-06-06 20:38:11 +0900

    Update SignInUI.cpp

--- SignInUI.cpp
+++ SignInUI.cpp
@@ -14,13 +14,27 @@
 #include "SignIn.h"
 
 
-
+// Function: SignInUI()
+// Description: Singleton을 위한 private 생성자
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignInUI::SignInUI()
 {
 }
 
-
+// Function: static SignInUI* var
+// Description: Singleton을 위한 private instance 변수
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignInUI* SignInUI::var = nullptr;
+
+// Function: static SignInUI& get()
+// Description: Singleton을 위한 instance caller
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignInUI & SignInUI::get()
 {
 	if (!var)
@@ -28,11 +42,21 @@ SignInUI & SignInUI::get()
 	return *var; //instance caller
 }
 
+// Function: const Info* requestSignIn(std::string id, std::stringpw)
+// Description: 로그인을 담당하는 boundary method
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 const Info * SignInUI::requestSignIn(std::string id, std::string pw)
 {
 	return SignIn::get().putIdPw(id, pw); //로그인화면 UI로 제공
 }
 
+// Function: ~SignInUI()
+// Description: 소멸자
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignInUI::~SignInUI()
 {
 }
