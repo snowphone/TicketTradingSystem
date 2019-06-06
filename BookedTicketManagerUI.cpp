5a74d8a03c9d7dbacce2daeebfbaf3758f7c0631
soyeon27 <51394144+soyeon27@users.noreply.github.com>
2019-06-06 21:53:26 +0900

    Update BookedTicketManagerUI.cpp

--- BookedTicketManagerUI.cpp
+++ BookedTicketManagerUI.cpp
@@ -45,6 +45,7 @@ BookedTicketManagerUI & BookedTicketManagerUI::get()
 // Function: void search(const Info& buyerInfo);
 // Description: 예약 티켓 조회를 위한 boundary method
 // Author: 김소연
+// Parameters: const Info & buyerInfo -구
 // Revisions:
 //		1. When & Who: 2019/06/01 by 김소연
 void BookedTicketManagerUI::search(const Info & buyerInfo)
