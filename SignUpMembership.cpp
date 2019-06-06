7b32354905192136534ea4d7e92deb52b906cf8e
soyeon27 <51394144+soyeon27@users.noreply.github.com>
2019-06-06 20:59:14 +0900

    Update SignUpMembership.cpp

--- SignUpMembership.cpp
+++ SignUpMembership.cpp
@@ -1,14 +1,33 @@
 //Class: SignUpMembership
 //Description: 회원가입을 담당하는 control class이다.
 //Author: 김소연
+// Created: 2019/06/01
+// Mail: 96syk@naver.com
+
+// Revisions:
+// 		1.	When & Who: 2019/06/01 by 김소연
+// 				What: create file
+
 #include <iostream>
 
 #include "UserCollection.h"
 #include "SignUpMembership.h"
 #include "Seller.h"
 
+
+// Function: static SignUpMembership* var
+// Description: singleton을 위한 private instance 변수
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignUpMembership* SignUpMembership::var = nullptr;
 
+
+// Function: static SignUpMembership& get()
+// Description:Singleton 패턴을 위한 instance caller
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignUpMembership&  SignUpMembership::get()
 {
 	if (!var)
@@ -16,6 +35,13 @@ SignUpMembership&  SignUpMembership::get()
 	return *var;
 }
 
+
+
+// Function: void signUp(const Info& info)
+// Description: 회원가입을 수행하는 control method
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 void SignUpMembership::signUp(const Info & info)
 {
 	if (info.getUserType() == "buyer") {
@@ -31,12 +57,22 @@ void SignUpMembership::signUp(const Info & info)
 }
 
 
-
+// Function: SignUpMembership()
+// Description: Singleton 패턴을 위한 private 생성자
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignUpMembership::SignUpMembership()
 {
 }
 
 
+
+// Function: ~SignUpMembership()
+// Description: 소멸자
+// Author: 김소연
+// Revisions:
+//		1. When & Who: 2019/06/01 by 김소연
 SignUpMembership::~SignUpMembership()
 {
 }
